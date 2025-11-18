83d4500b74817dde6806d7bfd8e6a997e212a01f
Cris <cristian@contractingempire.com>
2025-11-18 17:41:57 +0000

    Point job events to existing Pub/Sub topic

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const JOBS_TABLE_ID = 'client_audits_jobs';
 
 // Pub/Sub client
 const pubsub = new PubSub({ projectId: PROJECT_ID });
-const JOB_EVENTS_TOPIC = 'client-audits-job-events';
+const JOB_EVENTS_TOPIC = 'client-audits-job';
 
 // ---------- HEALTH CHECK ----------
 app.get('/', (req, res) => {
